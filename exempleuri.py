5b7897c33905a29755666689caa7f6f0f248b1eb
robertomacedo <macedorss20@gmail.com>
2021-11-09 14:30:00 -0300

    média ponderada 2

--- exempleuri.py
+++ exempleuri.py
@@ -40,6 +40,30 @@ and after the equal signal"""
 A = float(5.0)
 B = float(7.1)
 
-MEDIA = ((A*3.5) + (B*7.5))/(3.5 + 7.5)
+MEDIA = (A*3.5 + B*7.5)/(3.5 + 7.5)
 
 print(f'MEDIA = {MEDIA:.5f}')
+
+
+"""
+Read three values (variables A, B and C), which are the three student's grades. Then, 
+calculate the average, considering that grade A has weight 2, grade B has weight 3 and 
+the grade C has weight 5. Consider that each grade can go from 0 to 10.0, always with one 
+decimal place.
+
+Input
+The input file contains 3 values of floating points (double) with one digit after 
+the decimal point.
+
+Output
+Print the message "MEDIA"(average in Portuguese) and the student's average according to 
+the following example, with a blank space before and after the equal signal.
+"""
+
+A = float(5.0)
+B = float(10.0)
+C = float(10.0)
+
+MEDIA = (A*2 + B*3 + C*5)/(2 + 3 + 5)
+
+print('MEDIA = ', MEDIA)
