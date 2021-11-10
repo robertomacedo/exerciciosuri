8af9910c6472d3724c03fe22b3c77cbf16e78b4f
robertomacedo <macedorss20@gmail.com>
2021-11-10 14:51:57 -0300

    resolvido calculo de áreas 1012

--- volume1011.py
+++ volume1011.py
@@ -22,3 +22,39 @@ pi = 3.14159
 result = (4.0/3) * pi * (raio ** 3)
 
 print(f'VOLUME = {result:.3f}')
+
+
+"""
+Make a program that reads three floating point values: A, B and C. Then, calculate and show:
+a) the area of the rectangled triangle that has base A and height C.
+b) the area of the radius's circle C. (pi = 3.14159)
+c) the area of the trapezium which has A and B by base, and C by height.
+d) the area of ​​the square that has side B.
+e) the area of the rectangle that has sides A and B.
+
+Input
+The input file contains three double values with one digit after the decimal point.
+
+Output
+The output file must contain 5 lines of data. Each line corresponds to one of the 
+areas described above, always with a corresponding message (in Portuguese) and one 
+space between the two points and the value. The value calculated must be presented 
+with 3 digits after the decimal point
+"""
+
+dado = input().split()
+a = float(dado[0])
+b = float(dado[1])
+c = float(dado[2]) 
+
+tri = a*c/2
+cir = 3.14159 * (c ** 2)
+tra = (a+b)*c/2
+squ = b*b
+ret = a*b
+
+print(f'TRIANGULO: {tri:.3f}')
+print(f'CIRCULO: {cir:.3f}')
+print(f'TRAPEZIO: {tra:.3f}')
+print(f'QUADRADO: {squ:.3f}')
+print(f'RETANGULO: {ret:.3f}')
