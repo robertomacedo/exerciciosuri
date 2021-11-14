607b1e991473e4b499b265e4d0b222bc89bb3bbd
robertomacedo <macedorss20@gmail.com>
2021-11-14 17:00:13 -0300

    desafio bhaskara

--- bee1037.py
+++ bee1037.py
@@ -1,3 +1,4 @@
+import math
 """
 You must make a program that read a float-point number and print 
 a message saying in which of following intervals the number belongs: 
@@ -9,15 +10,34 @@ The symbol '(' represents greather than. For example:
 [0,25] indicates numbers between 0 and 25.0000, including both.
 (25,50] indicates numbers greather than 25 (25.00001) up to 50.0000000.
 """
-valor = float(input())
+# valor = float(input())
 
-if 0<=valor<=25:
-    print('Intervalo [0, 25]')
-elif 25<valor<=50:
-    print('Intervalo (25,50]')
-elif 50<valor<=75:
-    print('Intervalo (50,75]')
-elif 75<valor<=100:
-    print('Intevalo (75,100]')
-else:
-    print('Fora de intervalo')
+# if 0<=valor<=25:
+#     print('Intervalo [0, 25]')
+# elif 25<valor<=50:
+#     print('Intervalo (25,50]')
+# elif 50<valor<=75:
+#     print('Intervalo (50,75]')
+# elif 75<valor<=100:
+#     print('Intevalo (75,100]')
+# else:
+#     print('Fora de intervalo')
+
+r = input().split()
+
+a = float(r[0])
+b = float(r[1])
+c = float(r[2])
+
+delta = (b ** 2) - (4 * a * c)
+
+if (delta < 0):
+    print('Impossivel calcular')
+
+
+if (delta > 0):
+    x1 = (-b + math.sqrt(delta)) / (2 * a)
+    x2 = (-b - math.sqrt(delta)) / (2 * a)
+
+    print(f"R1 = {x1:.5f}")
+    print(f"R2 = {x2:.5f}")
