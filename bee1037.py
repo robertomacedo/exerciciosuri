d59c553b34bb4c9c32df61a0f13a119ebe01f28d
robertomacedo <macedorss20@gmail.com>
2021-11-18 20:17:12 -0300

    atualização de cod

--- bee1037.py
+++ bee1037.py
@@ -10,28 +10,24 @@ The symbol '(' represents greather than. For example:
 [0,25] indicates numbers between 0 and 25.0000, including both.
 (25,50] indicates numbers greather than 25 (25.00001) up to 50.0000000.
 """
-valor = float(input())
+# valor = float(input())
 
-if 0<=valor<=25:
-    print('Intervalo [0, 25]')
-elif 25<valor<=50:
-    print('Intervalo (25,50]')
-elif 50<valor<=75:
-    print('Intervalo (50,75]')
-elif 75<valor<=100:
-    print('Intevalo (75,100]')
-else:
-    print('Fora de intervalo')
+# if 0<=valor<=25:
+#     print('Intervalo [0, 25]')
+# elif 25<valor<=50:
+#     print('Intervalo (25,50]')
+# elif 50<valor<=75:
+#     print('Intervalo (50,75]')
+# elif 75<valor<=100:
+#     print('Intevalo (75,100]')
+# else:
+#     print('Fora de intervalo')
 
 """
 formula de bhaskara
 """
 
-r = input().split()
-
-a = float(r[0])
-b = float(r[1])
-c = float(r[2])
+a, b, c = map(float, input().split())
 
 delta = (b ** 2) - (4 * a * c)
 
